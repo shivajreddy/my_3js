31ad2ff402163f6b13714db8e1fdf2e1ebf95ddd
shiva <sreddy@hey.com>
2025-06-16 15:50:49 -0400

    added 3js react-fiber

--- pilot/src/App.tsx
+++ pilot/src/App.tsx
@@ -1,12 +1,23 @@
+// App.tsx
 import './App.css'
+import { Canvas } from '@react-three/fiber';
 
-function App() {
-
+function Box() {
     return (
-        <>
-            <p>3js</p>
-        </>
-    )
+        <mesh rotation={[90, 0, 20]}>
+            <boxGeometry />
+            <meshStandardMaterial color="orange" />
+        </mesh>
+    );
 }
 
+function App() {
+    return (
+        <Canvas>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Box />
+        </Canvas>
+    );
+}
 export default App
